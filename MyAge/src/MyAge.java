b9c74abf0b2aa5edd05f38c778344de85648c883
pc001 <en0605taku@gmail.com>
2021-05-07 15:16:28 +0900

    Add space

--- MyAge/src/MyAge.java
+++ MyAge/src/MyAge.java
@@ -3,7 +3,7 @@
  */
 public class MyAge {
     // ここから main メソッド
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int age;    // 年齢を入れる箱
         age = 19;
         System.out.println("私は" + age + "歳");
