ad8106fc870ebd76c572a8a45a0849a09b71ee7f
pc001 <en0605taku@gmail.com>
2021-05-07 14:00:43 +0900

    Add MyAge

---
+++ MyAge/src/MyAge.java
@@ -0,0 +1,11 @@
+/*  サンプルプログラム MyAge
+    開発者: 遠藤  作成日: 2021年5月7日
+ */
+public class MyAge {
+    // ここから main メソッド
+    public static void main(String[] args){
+        int age;    // 年齢を入れる箱
+        age = 19;
+        System.out.println("私は" + age + "歳");
+    }
+}
